dc589f44bd0ba24637815eb757c655925748667c
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-04-13 11:35:51 +0200

    added routers

--- server/index.js
+++ server/index.js
@@ -7,6 +7,21 @@ const PORT = 3000;
 // Serve everything from public/
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/home.html"))
+  console.log("Entered in home app")
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/about.html"))
+  console.log("entered about")
+})
+
+app.get("/timzeones", (req, res) => {
+  res.sendFile(path.join(__dirname, "timezones.html"))
+  console.log("entered timezones")
+})
+
 app.listen(PORT, () => {
   console.log(`✨ SamePages server running at http://localhost:${PORT}`);
 });
