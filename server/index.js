a70a14906cfa4039eb48985da538835c8bd77b9d
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-04-13 11:07:42 +0200

    website

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve everything from public/
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(PORT, () => {
+  console.log(`✨ SamePages server running at http://localhost:${PORT}`);
+});
